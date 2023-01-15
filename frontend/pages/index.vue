95bb0a6de6cfbed0440b07d4c80f0ea10f095862
Jade Nash <jade@lilydev.com>
2023-01-15 13:53:00 -0600

    make .env and fetch data from backend in vue

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,11 +1,30 @@
 <template>
-  <Tutorial />
+	<div>
+		<p v-if="$fetchState.pending">Loading</p>
+		<p v-if="$fetchState.error">Error occured</p>
+		<div v-else v-for="post in posts_data.posts" :key="post">
+			<h1>
+				{{ post.title }}
+			</h1>
+		</div>
+
+
+	</div>
 </template>
 
 <script lang="ts">
-import Vue from 'vue'
+	import Vue from "vue";
 
-export default Vue.extend({
-  name: 'IndexPage',
-})
+	export default Vue.extend({
+		data() {
+			return {
+				posts_data: {}
+			}
+		},
+		async fetch() {
+			this.posts_data = await fetch(
+				"http://localhost:8081/api/posts"
+			).then(res => res.json())
+		}
+	})
 </script>
